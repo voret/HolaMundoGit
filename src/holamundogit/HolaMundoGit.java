97b8a2043dede098ea74325f927892c2c460407c
a029175845t <a029175845t@s09-pc21>
2015-11-11 20:03:31 +0100

    version 1

---
+++ src/holamundogit/HolaMundoGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package holamundogit;
+
+/**
+ *
+ * @author a029175845t
+ */
+public class HolaMundoGit 
+{
+  public static void main(String[] args) 
+    {
+     System.out.println("Hola Mundo");
+     System.out.println("ROJO");
+    }
+    
+}
